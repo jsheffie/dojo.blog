aaf4435b0b1137c109e9966725bab1a11bde4f94
Jeff Sheffield <jeff.sheffield@gmail.com>
2023-08-27 15:00:49 -0500

    updatable state

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,17 @@
 import { useState } from "react";
 const Home = () => {
     // let name = "mario";
-    const [name, setName] = useState('mario')
-
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
     const handleClick = () => {
         setName('luigi'); // now name is reactive
+        setAge(30);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is { age } years old</p>
             <button onClick={handleClick}>Click Me</button>
         </div>
      );
