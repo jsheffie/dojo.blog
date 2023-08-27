d02c96a40414c7800980a36dee445f988fb49871
Jeff Sheffield <jeff.sheffield@gmail.com>
2023-08-27 11:27:45 -0500

    #7 click events ( multiple line anonymous function)

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("Hello, ninjas!", "This is a test");
+    }
+    const handleClickAgain = (name) => {
+        console.log("Hello " + name);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => {
+                handleClickAgain("mario")
+            }} >Click Me Again</button>
         </div>
      );
 }
