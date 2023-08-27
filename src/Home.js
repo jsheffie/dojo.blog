8fe245ffbfb6ecdd5361ef999128ce40c59af078
Jeff Sheffield <jeff.sheffield@gmail.com>
2023-08-27 13:52:52 -0500

    the beginning of ch 8 useState

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,16 @@
 const Home = () => {
-    const handleClick = (e) => {
-        console.log("Hello, ninjas!", e);
-    }
-    const handleClickAgain = (name, e) => {
-        console.log("Hello " + name, e.target);
+    let name = "mario";
+
+    const handleClick = () => {
+        name = 'luigi';
+        console.log(name);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => handleClickAgain("mario", e)} >Click Me Again</button>
         </div>
      );
 }
